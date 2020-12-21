6d06901219042c93e3d9d21d09c4d2210931c901
MalC-U <mwcalow@gmail.com>
2020-12-21 16:33:25 +0000

    Added function that decreases max height of spinning wheel when scrolling donw the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,15 @@ function jumpToServices() {
         behavior: "smooth" 
         });    
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("wheel").style.maxHeight = "3rem";
+  } else {
+    document.getElementById("wheel").style.maxHeight = "7rem";
+
+  }
+}
