21f25cdb3c3fa154fad37a75b0c85c170f949f2a
MalC-U <mwcalow@gmail.com>
2020-12-22 20:37:07 +0000

    emailjs js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,40 @@ function scrollFunction() {
   $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+
+/*
+
+EmailJS script to be completed
+
+
+const contactForm = document.querySelector('#contact-form');
+
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = {
+        service_id: 'YOUR_SERVICE_ID',,
+        template_id: 'YOUR_TEMPLATE_ID',
+        user_id: 'YOUR_USER_ID',
+        template_params: {
+            "name": contactForm.name.value,
+            "email": contactForm.email.value,
+            "message": contactForm.message.value
+        }
+    };
+
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function () {
+        $('input').val('');
+        $('textarea').val('');
+        alert('Your email has been sent successfully!');
+        window.location.reload(true); 
+    }).fail(function (error) {
+        console.log('Oops... ' + JSON.stringify(error));
+        alert('Oops something went wrong, please try again');
+    });
+});
+*/
